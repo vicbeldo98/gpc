7993a4ef51f7c5304cedc76f92984653cb2bc1c5
victoria <vikibeldo@hotmail.es>
2021-09-09 10:51:38 +0200

    fixing bug

--- threejs/test4.js
+++ threejs/test4.js
@@ -1,6 +1,6 @@
 //  SHADER DE VERTICES
 var VSHADER_SOURCE =
-    'void main(){ gl_Position = vec4(0.0,0.0,0.0,1.0); glPointSize = 10.0;}'; 
+    'void main(){ gl_Position = vec4(0.0,0.0,0.0,1.0); gl_PointSize = 10.0;}'; 
 
 //  SHADER DE FRAGMENTOS
 var FSHADER_SOURCE = 'void main(){ gl_FragColor = vec4( 1.0, 0.0, 0.0, 1.0);}';
