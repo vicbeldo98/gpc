f62a0a368ae08a1316a106fd411b4152bd05eea8
victoria <vikibeldo@hotmail.es>
2021-09-13 11:40:39 +0200

    prac1 acabada

--- threejs/prac1.js
+++ threejs/prac1.js
@@ -1,110 +1,110 @@
-/*
-Práctica 1. Clickar puntos con Webgl
-*/
+let VSHADER_SOURCE =
+    'attribute vec2 position;\n' + // attribute variable
+    'attribute vec3 color;                        \n'+
+    'varying highp vec3 vColor;                   \n'+
+    'void main() {\n' +
+    'gl_Position = vec4(position, 0.0, 1.0);\n' +
+    'gl_PointSize = 10.0;\n' +
+    'vColor = color;\n' +
+    '}';
 
-// SHADER DE VERTICES
-var VSHADER_SOURCE =
-    'attribute vec3 posicion;                 \n' +
-    'void main(){                             \n' +
-    '  gl_Position = vec4(posicion,1.0);      \n' +
-    '  gl_PointSize = 10.0;                   \n' +
-    '}                                        \n'
- 
-// SHADER DE FRAGMENTOS
-var FSHADER_SOURCE =
-'uniform highp vec3 color;                \n' +
-'void main(){                             \n' +
-'  gl_FragColor = vec4(color,1.0);        \n' +
-'}                                        \n'
+let FSHADER_SOURCE =
+    'precision mediump float;\n' +
+    'varying highp vec3 vColor;\n' +
+    'void main(){  \n' +
+    'gl_FragColor = vec4 ( vColor, 1.0 ); \n' +
+    '}\n';
 
-// Globales
-var clicks = [];
-var colorFragmento;
+let bufferCoordenadas = null;
+let bufferColors = null;
 
-function main()
-{
-    // Recupera el canvas
-    var canvas = document.getElementById("canvas");
-    if( !canvas ){
-        console.log("Fallo en el canvas");
-        return;
+function main() {
+
+    let canvas = document.getElementById('canvas');
+
+    if (!canvas) {
+        console.log("ERROR CANVAS");
+        return ;
     }
 
-    // Asigna el contexto grafico
-    var gl = getWebGLContext( canvas );
-    if( !gl ){
-        console.log("Fallo el contexto grafico");
-        return;
+    let gl = getWebGLContext(canvas);
+
+    if (!gl) {
+        console.log("ERROR GL CONTEXT");
+        return ;
     }
 
-    // Carga, compila y monta los shaders
-    if ( !initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE) ){
-        console.log("Fallo la carga de los shaders");
-        return;
-    } 
+    if ( !initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE) ) {
+        console.log("ERROR INIT SHADERS");
+        return ;
+    }
 
-    // Color de fondo
-    gl.clearColor( 0.0, 0.0, 0.2, 1.0 );
+    gl.clearColor( 0.0, 0.0, 0.5, 1.0 );
 
-    // Localiza el atributo 'posicion' en shader
-    var coordenadas = gl.getAttribLocation( gl.program, 'posicion');
+    gl.clear( gl.COLOR_BUFFER_BIT );
 
-    // Crear el buffer, seleccionarlo y activar la conexion
-    var bufferVertices = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferVertices );
-    gl.vertexAttribPointer( coordenadas, 3, gl.FLOAT, false, 0, 0 );
+    //  Localiza la variable position en el segment shader
+    let coordenadas = gl.getAttribLocation( gl.program, 'position' );
+
+    //  Crea buffer para positions
+    bufferCoordenadas = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferCoordenadas );
+    gl.vertexAttribPointer( coordenadas, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( coordenadas );
 
-    // Localiza la variable 'color' en el shader
-    colorFragmento = gl.getUniformLocation( gl.program, 'color' );
+    //  Localiza la variable color en el segment shader
+    let color = gl.getAttribLocation( gl.program, 'color');
 
-    // Registrar la callback de click de raton
-    canvas.onmousedown = function( evento ){
-        click( evento, gl, canvas );
-    }
+    //  Crea buffer para colores
+    bufferColors = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferColors );
+    gl.vertexAttribPointer( color, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( color );
 
-    // Dibujar
-    render( gl );
-    
+    canvas.onmousedown = function( evento ) { click( evento, gl, canvas ); };
+
 }
 
-function click( evento, gl, canvas )
-{
-    // Leer la coordenada del click. Sistema de referencia del documento
-    var x = evento.clientX;
-    var y = evento.clientY;
+let puntos = [];
+let puntos_colors = [];
 
-    // Situación del canvas en el documento
-    var rect = evento.target.getBoundingClientRect();
+function click( evento, gl, canvas ) {
 
-    // Sistema de referencia del documento:
-    //   Origen: TopLeft del area cliente
-    //   Ejes: X+ derecha, Y+ abajo
-    //   Dimensiones: ancho x alto (px) del documento
-    //
-    // Sistema de referencia de Webgl (por defecto)
-    //   Origen: Centro
-    //   Ejes: X+ derecha, Y+ arriba
-    //   Dimensiones: 2x2 en R2 ajustado al canvas
+    let x = evento.clientX;
+    let y = evento.clientY;
 
-    // Transformar s.r. documento a s.r. Webgl
+    let rect = evento.target.getBoundingClientRect();
+
     x = ((x - rect.left) - canvas.width/2) * 2/canvas.width;
     y = (canvas.height/2 - (y - rect.top)) * 2/canvas.height;
 
-    // Guardar el punto (x,y,z) y dibujar la lista
-    clicks.push(x); clicks.push(y); clicks.push(0.0);
-    render(gl);
+    let distance_to_origin = Math.sqrt(x*x + y*y);
+
+    let color_v = 1-(distance_to_origin/Math.sqrt(2));
+
+    for (let i = 0; i < 3; i ++ ) puntos_colors.push(color_v);
+
+    puntos.push(x);
+    puntos.push(y);
+
+    render( gl );
+
 }
 
-function render( gl )
-{
-    // Borrar el canvas
+function render( gl ) {
+
+    let puntos_array = new Float32Array(puntos);
+    let colors_array = new Float32Array(puntos_colors);
+
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // Todos los puntos del mismo color
-    gl.uniform3f( colorFragmento, 1, 1, 0);
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferCoordenadas );
+    gl.bufferData( gl.ARRAY_BUFFER, puntos_array, gl.STATIC_DRAW );
 
-    // Rellenar el VBO y mandar al shader
-    gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( clicks ), gl.STATIC_DRAW );
-    gl.drawArrays( gl.POINTS, 0, clicks.length/3 );
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferColors );
+    gl.bufferData( gl.ARRAY_BUFFER, colors_array, gl.STATIC_DRAW );
+
+    gl.drawArrays( gl.POINTS, 0, puntos_array.length/2 );
+    gl.drawArrays( gl.LINE_STRIP, 0, puntos_array.length/2 );
+
 }
