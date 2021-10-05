0dd86e457f088d0f1f433e25a251ccbe011a610f
victoria <vikibeldo@hotmail.es>
2021-10-05 09:26:53 +0200

    audio

--- proyecto_final/proyecto_personal.js
+++ proyecto_final/proyecto_personal.js
@@ -170,22 +170,22 @@ function arrowGeometry(material){
 }
 
 function loadSong(){
+    //document.getElementById('darude').play();
     // create an AudioListener and add it to the camera
     const listener = new THREE.AudioListener();
-    camera.add(listener);
+    camera.add( listener );
 
     // create a global audio source
-    const sound = new THREE.Audio(listener);
+    const sound = new THREE.Audio( listener );
 
     // load a sound and set it as the Audio object's buffer
     const audioLoader = new THREE.AudioLoader();
-    audioLoader.load( 'songs/darude_sandstorm.ogg', function( buffer ) {
+    audioLoader.load( 'proyecto_final/songs/darude_sandstorm.ogg', function( buffer ) {
         sound.setBuffer( buffer );
-        sound.setLoop( false );
+        sound.setLoop( true );
         sound.setVolume( 0.5 );
         sound.play();
     });
-
     //loadSongArrows();
 }
 
